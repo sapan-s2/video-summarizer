51d640e63f2d88bb372071b50a332145dce842d9
sapan jain <jsapan4@gmail.com>
2025-04-01 15:54:07 -0500

    updating port in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+import os
 from fastapi import FastAPI
 from app.routers import video_upload, summarize, youtube_download
 
 app = FastAPI()
+
+port = int(os.environ.get("PORT", 5000))
+# Use uvicorn to run the app when executed directly
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # Include routers
 app.include_router(video_upload.router, prefix="/video", tags=["Video Upload"])
